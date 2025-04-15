a2785a9dfbf835044ccf8187799a27200bf154b7
KK63030 <kamlesh.63030@iqra.edu.pk>
2025-04-15 16:40:43 +0500

    Update Q2.java

--- Q2.java
+++ Q2.java
@@ -1,13 +1,22 @@
-class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome To Software Construction!");
-        
-        int sum;
-        int a=5;
-        int b=4;
-        sum= a+b;
-        System.out.println("Sum of A and b is "+sum);
-      
+public class ModularExample {
 
+    // Method to print a message
+    public static void printMessage() {
+        System.out.println("Welcome to Software Construction!");
+    }
+
+    // Method to calculate sum of two integers
+    public static int calculateSum(int a, int b) {
+        return a + b;
+    }
+
+    // Main method to call the modular functions
+    public static void main(String[] args) {
+        // Call the message printing method
+        printMessage();
+
+        // Call the sum calculation method
+        int result = calculateSum(7, 8);
+        System.out.println("The sum is: " + result);
     }
 }
