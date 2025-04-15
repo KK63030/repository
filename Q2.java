698ed145afc1d60b0ce444e96dfd73ecf842b85f
KK63030 <kamlesh.63030@iqra.edu.pk>
2025-04-15 16:03:52 +0500

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Software Construction!");
+        
+        int sum;
+        int a=5;
+        int b=4;
+        sum= a+b;
+        System.out.println("Sum of A and b is "+sum);
+      
+
+    }
+}
